a8eed154985abfe008bb94fbdef003fd8d63bf72
Martin Dib <martin.dib2005@gmail.com>
2024-10-14 19:51:00 -0300

    adicion del mapa

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -32,7 +32,7 @@ export class Preloader extends Scene
         //  Load the assets for the game - Replace with your own assets
         this.load.setPath('assets');
         this.load.image('logo', 'logo.png');
-        this.load.image('mapa', 'mapafinal.png');
+        this.load.image('mapa', 'mapaslabcuadrada.png');
         this.load.spritesheet('ataque', 'boladefuegospritesheet.png', {
             frameWidth: 50, // Ancho de cada frame del spritesheet
             frameHeight: 45 // Alto de cada frame del spritesheet
