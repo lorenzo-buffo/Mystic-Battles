2e0c5ab5ea6b40e22a309bf64776d2514445e293
lorenzo-buffo <lolitobuffo@gmail.com>
2025-02-26 16:31:41 -0300

    .

--- src/Services/firestore.js
+++ src/Services/firestore.js
@@ -1,22 +1,24 @@
 // src/services/firestore.js
-import { db } from './firebaseConfig';
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from './firebaseConfig';//Importa db desde firebaseConfig.js, que es la instancia de Firestore.
+import { collection, addDoc, getDocs } from "firebase/firestore";//Importa funciones de Firestore para manejar documentos en la base de datos.
 
-const scoresCollection = collection(db, "scores");
+const scoresCollection = collection(db, "scores");//Crea una referencia a  scores en la base de datos de Firestore.
 
-export const addScore = async (name, points) => {
+
+export const addScore = async (name, points) => {//Guarda una nueva puntuación en Firestore.
   try {
-    await addDoc(scoresCollection, { name, points });
+    await addDoc(scoresCollection, { name, points });//Usa addDoc() para insertar estos datos en scores
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
 
-export const getScores = async () => {
+export const getScores = async () => {//Usa getDocs() para recuperar los documentos de scores
   const querySnapshot = await getDocs(scoresCollection);
-  const scores = [];
+  const scores = [];//Recorre cada documento y lo agrega al arreglo scores
   querySnapshot.forEach((doc) => {
     scores.push(doc.data());
   });
-  return scores;
+  return scores;//Retorna un arreglo con todas las puntuaciones.
 };
+
--- src/Services/translation.js
+++ src/Services/translation.js
@@ -6,28 +6,30 @@ const PROJECT_ID = 'cm22m0m9k0002ewmloapfvv3n';
 let translations = null;
 let language = ES_AR;
 
-export async function getTranslations(lang, callback) {
-    localStorage.clear();
-    translations = null;
-    language = lang;
+
+export async function getTranslations(lang, callback) {//Esta función obtiene las traducciones desde la API y las almacena en localStorage.
+    localStorage.clear();//borra cualquier traduccion almacenada en el navegador
+    translations = null; //resetea la variable translations eliminando traducciones previas
+    language = lang; //cambia el idioma actual al que se paso como argumento
     if (language === ES_AR) {
         return callback ? callback() : false;
     }
+    
+   
 
-    return await fetch(`https://traducila.vercel.app/api/translations/${PROJECT_ID}/${language}`)
-    .then(response => response.json())
+    return await fetch(`https://traducila.vercel.app/api/translations/${PROJECT_ID}/${language}`)// si el idioma no es español hace la solicitud a la api
+    .then(response => response.json())//  la respuesta se convierte en formato json
     .then(data => {
-        localStorage.setItem('translations', JSON.stringify(data));
-        translations = data;
-        if(callback) callback()
+        localStorage.setItem('translations', JSON.stringify(data));// guarda las traducciones en en el almacenamiento local del navegador
+        translations = data;//Guarda las traducciones en la variable
+        if(callback) callback()//Si se proporcionó un callback, lo ejecuta una vez que las traducciones han sido obtenidas
     });
 }
 
 
 
 
-
-export function getPhrase(key) {
+export function getPhrase(key) { // funcion devuelve la traduccion correspondiente a una clave de terxto
     if (!translations) {
         const locals = localStorage.getItem('translations');
         translations = locals ? JSON.parse(locals) : null;
@@ -51,26 +53,18 @@ function isAllowedLanguge(language) {
     return allowedLanguages.includes(language);
 }
 
-export function getLanguageConfig() {
+
+export function getLanguageConfig() {//Esta función intenta determinar qué idioma debe usarse
     let languageConfig;
 
     // Obtener desde la URL el idioma
     console.log(window.location.href)
 
-    /* 
-      depende como lo manejemos: 
-      1) puede venir como www.dominio.com/es-AR
-      2) puede venir como www.dominio.com?lang=es-AR
-
-      En el primer caso se obtiene con: window.location.pathname
-      En el segundo caso se obtiene leyendo el query param lang 
-      
-      vamos a implementar una logica que cubra ambos casos
-    */
-
-    const path = window.location.pathname !== '/' ? window.location.pathname : null;
-    const params = new URL(window.location.href).searchParams;
-    const queryLang = params.get('lang');
+    const path = window.location.pathname !== '/' ? window.location.pathname : null;// Obtiene la parte de la URL que indica la ruta de la página
+    const params = new URL(window.location.href).searchParams;//Obtiene los parámetros de la URL
+    const queryLang = params.get('lang');//Busca el valor del parámetro lang en la URL.
+ 
+    
 
     languageConfig = path ?? queryLang;
 
@@ -79,11 +73,12 @@ export function getLanguageConfig() {
             return languageConfig;
         }
     }
-
-    const browserLanguage = window.navigator.language;
+    //Si languageConfig tiene un valor y está en la lista de idiomas permitidos, lo devuelve como idioma seleccionado.
+    const browserLanguage = window.navigator.language;//Obtiene el idioma del navegador del usuario
     if (isAllowedLanguge(browserLanguage)) {
         return browserLanguage;
     }
 
     return ES_AR;
+    //Si no se encontró un idioma válido en la URL ni en el navegador, usa ES_AR
 }
